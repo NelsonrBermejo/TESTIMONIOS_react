a5d92f2a34cb6834bd71cdc630aaaa88a6f010a7
NelsonrBermejo <bermejonelson.r@gmail.com>
2023-06-27 13:03:38 -0300

    Hook: useState para actualizar el número de Clics

--- contador-de-clics/src/App.js
+++ contador-de-clics/src/App.js
@@ -2,11 +2,29 @@ import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-import { manejarClic } from './componentes/BotonContador';
-import { reiniciarContador } from './componentes/BotonContador';
-
+import { useState } from 'react';
 
 function App() {
+
+/**
+ * inicialmente número de clics vale Cero se lo paso en useState
+ */
+const [numClics, setNumClics] = useState(0);
+
+/**
+ * asigna nuevo valor a numClics
+ * @setNumClics(numClics +1) numero de Clics más uno;
+ */
+ const manejarClic = () => {
+  //console.log('Clic');
+  setNumClics(numClics +1);
+}
+
+ const reiniciarContador = () => {
+  console.log('Reiniciar Contador');
+}
+
+
 
   return (
     <div className='App'>
@@ -16,7 +34,7 @@ function App() {
         alt='Logo de freeCodeCamp' />
       </div>
       <div className='contenedor-principal-contador-botones'>
-        <Contador numClics='5' />
+        <Contador numClics={numClics} />
         <Boton 
           texto='Clic' 
           esBotonDeClic={true}
--- contador-de-clics/src/componentes/Contador.js
+++ contador-de-clics/src/componentes/Contador.js
@@ -1,6 +1,11 @@
 import React from "react";
 import '../stylesheets/Contador.css'
 
+/**
+ * 
+ * @param numClics el valore de numero de clics 
+ * @returns muestra el valor actual de numClics
+ */
 function Contador({	numClics })	{
 	return (
 		<div className='contador'>
