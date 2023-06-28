e204159864fb729af7cf15d79616a2b4a9b93d28
NelsonrBermejo <bermejonelson.r@gmail.com>
2023-06-28 13:26:04 -0300

    logo jsx y css

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import freeCodeCampImagen from './imagenes/freecodecamp-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      Hello world!
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          src={freeCodeCampImagen}
+          className='freecodecamp-logo'
+          alt='Logo de freeCodeCamp'
+        />
+
+      </div>
     </div>
   );
 }
