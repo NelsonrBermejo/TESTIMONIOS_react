ce1931c035e6072282950e6d4634646c4d5d15e5
NelsonrBermejo <bermejonelson.r@gmail.com>
2023-06-29 15:05:34 -0300

    componente pantalla

---
+++ calculadora/src/componentes/Pantalla.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../stylesheets/Pantalla.css';
+
+/**
+ * @FuncionFlecha se reemplaza el function Pantalla ({})  por const Pantalla = ()
+ * @param {*} Input es el valor ingresado por el usuario 
+ * @returns muestro en pantalla el valor ingresado por el usuario
+ */
+const Pantalla = (input) => (
+  <div className='input'>
+    {input}
+  </div>
+);
+
+export default Pantalla;
