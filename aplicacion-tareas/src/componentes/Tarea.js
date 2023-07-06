ce7437eb991e8fced000c425e5c2ab265cce1e08
NelsonrBermejo <bermejonelson.r@gmail.com>
2023-07-06 16:00:41 -0300

    icono

--- aplicacion-tareas/src/componentes/Tarea.js
+++ aplicacion-tareas/src/componentes/Tarea.js
@@ -1,5 +1,7 @@
 import React from "react";
-import '../hojas-de-estilo/Tarea.css'
+import '../hojas-de-estilo/Tarea.css';
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
 
 function Tarea ({ texto }) {
   return (
@@ -8,7 +10,7 @@ function Tarea ({ texto }) {
 				{texto}
 			</div>
 			<div className='tarea-icono'>
-				Eliminar
+				<AiOutlineCloseCircle />
 			</div>
     </div>
 
