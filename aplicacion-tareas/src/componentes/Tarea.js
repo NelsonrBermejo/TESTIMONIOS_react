e7deb7709e2602e45a4118c062b979b910e50d60
NelsonrBermejo <bermejonelson.r@gmail.com>
2023-07-07 13:43:45 -0300

    clase tarea completada

--- aplicacion-tareas/src/componentes/Tarea.js
+++ aplicacion-tareas/src/componentes/Tarea.js
@@ -3,9 +3,9 @@ import '../hojas-de-estilo/Tarea.css';
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
 
-function Tarea ({ texto }) {
+function Tarea ({ texto, completada }) {
   return (
-    <div className='tarea-contenedor'>
+    <div className={ completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
 			<div className='tarea-texto'>
 				{texto}
 			</div>
